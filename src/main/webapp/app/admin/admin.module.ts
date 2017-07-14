e18d2989e5ba22b82e856e7d1d106eede8ec4aa3
Marton Biro <marton.biro@mimacom.com>
2017-07-14 15:06:28 +0200

    JR2-63: Integrate ElasticSearch Reindexing into frontend

--- src/main/webapp/app/admin/admin.module.ts
+++ src/main/webapp/app/admin/admin.module.ts
@@ -2,6 +2,7 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { JobroomSharedModule } from '../shared';
+import { JobroomElasticsearchReindexModule } from './elasticsearch-reindex/elasticsearch-reindex.module';
 /* jhipster-needle-add-admin-module-import - JHipster will add admin modules imports here */
 
 import {
@@ -36,6 +37,7 @@ import {
     imports: [
         JobroomSharedModule,
         RouterModule.forRoot(adminState, { useHash: true }),
+        JobroomElasticsearchReindexModule,
         /* jhipster-needle-add-admin-module - JHipster will add admin modules here */
     ],
     declarations: [
