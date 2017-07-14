ca50493bfabdea921b9213fb6de619cadf1a935d
Marton Biro <marton.biro@mimacom.com>
2017-07-14 15:06:28 +0200

    JR2-63: Integrate ElasticSearch Reindexing into frontend

---
+++ src/main/webapp/app/admin/elasticsearch-reindex/elasticsearch-reindex.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+import { ElasticsearchReindexModalComponent } from './elasticsearch-reindex-modal.component';
+
+@Component({
+    selector: 'jhi-elasticsearch-reindex',
+    templateUrl: './elasticsearch-reindex.component.html'
+})
+export class ElasticsearchReindexComponent {
+
+    constructor(private modalService: NgbModal) {
+    }
+
+    showConfirm() {
+        this.modalService.open(ElasticsearchReindexModalComponent);
+    }
+}
