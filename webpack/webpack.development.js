5f4b2638ea1152dcc6eac23fc14fbc271c5e7edf
birom <marton.biro@gmail.com>
2017-09-12 15:32:40 +0200

    JR2-210: Fix styling issues
    -upgrade styleguide version
    -fix toolbar html structure
    -fix footer html structure
    -remove jhipster bootstrap customizations
    -fix styleguide integration into vendor.scss
    -update webpack config to use the correct dev resources

--- webpack/webpack.development.js
+++ webpack/webpack.development.js
@@ -1,5 +1,5 @@
 const commonConfig = require('./webpack.dev.js');
 
-commonConfig.devServer.proxy[0].target = 'http://dev.job-room.ch:8080';
+commonConfig.devServer.proxy[0].target = 'https://dev.job-room.ch:8443';
 
 module.exports = commonConfig;
