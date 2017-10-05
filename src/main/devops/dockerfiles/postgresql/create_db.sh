e1ba085ca3234fed9f255d35303790193073ff2e
Michael Wirth <michael.wirth@mimacom.com>
2017-10-05 12:51:18 +0000

    JR2-244 Deploy candidateservice

--- src/main/devops/dockerfiles/postgresql/create_db.sh
+++ src/main/devops/dockerfiles/postgresql/create_db.sh
@@ -6,4 +6,5 @@ POSTGRES="psql --username ${POSTGRES_USER}"
 $POSTGRES <<-EOSQL
 CREATE DATABASE jobservice OWNER jobservice;
 CREATE DATABASE referenceservice OWNER referenceservice;
+CREATE DATABASE candidateservice OWNER candidateservice;
 EOSQL
