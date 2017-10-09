67778832741b5f08e84faf03389eb3b29e961f59
birom <marton.biro@gmail.com>
2017-10-09 15:17:16 +0200

    JR2-321: Redesign login popup

--- src/main/webapp/app/shared/login/login-modal.service.ts
+++ src/main/webapp/app/shared/login/login-modal.service.ts
@@ -16,7 +16,8 @@ export class LoginModalService {
         }
         this.isOpen = true;
         const modalRef = this.modalService.open(JhiLoginModalComponent, {
-            container: 'nav'
+            container: 'nav',
+            size: 'lg'
         });
         modalRef.result.then((result) => {
             this.isOpen = false;
