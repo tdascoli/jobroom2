78280efab733f4c68628ae927dcefb91a87dc352
Patrick Ellenberger <p.ellenberger@outlook.com>
2017-10-19 13:05:01 +0200

    Fix missing library in IE
    The Zone.js library doesn't get loaded as a module
    and has to be included as a script tag (only for IE).
    
    This problem surely came with a new version of Angular.
    
    Source: https://stackoverflow.com/questions/39592949/angular2-final-release-error-angular-requires-zone-js-prolyfill

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -88,6 +88,7 @@ module.exports = (options) => {
             ),
             new CopyWebpackPlugin([
                 { from: './node_modules/core-js/client/shim.min.js', to: 'core-js-shim.min.js' },
+                { from: './node_modules/zone.js/dist/zone.min.js', to: 'zone.min.js' },
                 { from: './node_modules/swagger-ui/dist/css', to: 'swagger-ui/dist/css' },
                 { from: './node_modules/swagger-ui/dist/lib', to: 'swagger-ui/dist/lib' },
                 { from: './node_modules/swagger-ui/dist/swagger-ui.min.js', to: 'swagger-ui/dist/swagger-ui.min.js' },
