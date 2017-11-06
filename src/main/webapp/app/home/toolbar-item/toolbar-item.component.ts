59ff1bfed987f9b688b8510db8c51ba9d72742c8
Thomas D'Ascoli <thomas@dasco.li>
2017-11-06 15:09:19 +0100

    JR2-335: Fix

--- src/main/webapp/app/home/toolbar-item/toolbar-item.component.ts
+++ src/main/webapp/app/home/toolbar-item/toolbar-item.component.ts
@@ -3,7 +3,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 @Component({
     selector: 'jr2-toolbar-item',
     templateUrl: './toolbar-item.component.html',
-    styleUrls: ['./toolbar-item.component.css']
+    styleUrls: ['./toolbar-item.component.scss']
 })
 export class ToolbarItemComponent implements OnInit {
     @Input() icon: string;
