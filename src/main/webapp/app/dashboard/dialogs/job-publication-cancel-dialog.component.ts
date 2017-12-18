6f62dd925365afef005e620323acedefd1a572aa
birom <marton.biro@gmail.com>
2017-12-18 11:05:42 +0100

    JR2-546: Refactor cancelation dialog

--- src/main/webapp/app/dashboard/dialogs/job-publication-cancel-dialog.component.ts
+++ src/main/webapp/app/dashboard/dialogs/job-publication-cancel-dialog.component.ts
@@ -1,9 +1,26 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { JobPublicationService } from '../../shared/job-publication/job-publication.service';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ValidatorFn } from '@angular/forms';
 import { Subject } from 'rxjs/Subject';
 import { JobCancellationRequest } from '../../shared/job-publication/job-publication-cancel-request';
+
+const jobCancelFormValidator: ValidatorFn = (jobCancelForm: FormGroup) => {
+    const positionOccupiedValue = jobCancelForm.get('positionOccupied').value;
+    const jobCenterValue = jobCancelForm.get('occupiedWith.jobCenter').value;
+    const privateAgencyValue = jobCancelForm.get('occupiedWith.privateAgency').value;
+    const selfValue = jobCancelForm.get('occupiedWith.self').value;
+
+    if (!positionOccupiedValue) {
+        return null;
+    } else if (jobCenterValue || privateAgencyValue || selfValue) {
+        return null;
+    } else {
+        return {
+            occupiedWith: 'required'
+        }
+    }
+};
 
 @Component({
     selector: 'jr2-job-publication-cancel-dialog',
@@ -26,58 +43,23 @@ export class JobPublicationCancelDialogComponent implements OnInit, OnDestroy {
         this.jobCancelForm = this.fb.group({
             positionOccupied: true,
             occupiedWith: this.fb.group({
-                jobCenter: [false, Validators.requiredTrue],
-                privateAgency: [false, Validators.requiredTrue],
-                self: [false, Validators.requiredTrue]
+                jobCenter: [false],
+                privateAgency: [false],
+                self: [false]
             })
+        }, {
+            validator: jobCancelFormValidator
         });
-        this.initValueChangeListener();
-    }
 
-    private initValueChangeListener() {
-        this.jobCancelForm.controls['positionOccupied']
-            .valueChanges.takeUntil(this.unsubscribe$)
+        this.jobCancelForm.get('positionOccupied').valueChanges
+            .takeUntil(this.unsubscribe$)
             .subscribe((value) => {
                 if (value) {
-                    this.jobCancelForm.controls['occupiedWith'].enable();
+                    this.jobCancelForm.get('occupiedWith').enable();
                 } else {
-                    this.jobCancelForm.controls['occupiedWith'].disable();
+                    this.jobCancelForm.get('occupiedWith').disable();
                 }
             });
-        this.validateOccupiedWithCheckboxes()
-    }
-
-    private validateOccupiedWithCheckboxes() {
-        Object.keys(this.getOccupiedWithFormGroup().controls).forEach((controlName) => {
-            this.getOccupiedWithFormGroup().controls[controlName].valueChanges
-                .distinctUntilChanged()
-                .takeUntil(this.unsubscribe$)
-                .subscribe((value) => {
-                    const validators = this.isOccupiedWithHasValidValue(value) ? [] : [Validators.requiredTrue];
-                    this.setValidators(controlName, validators);
-                })
-        });
-    }
-
-    private isOccupiedWithHasValidValue(value: boolean) {
-        return value || Object.keys(this.getOccupiedWithFormGroup().controls)
-            .map((controlName) => this.getOccupiedWithFormGroup().controls[controlName])
-            .find((control) => (control.value as boolean)) !== undefined;
-
-    }
-
-    private setValidators(controlFiredValueChangedEvent: string, validators: ValidatorFn[]) {
-        Object.keys(this.getOccupiedWithFormGroup().controls)
-            .filter((controlName) => controlName !== controlFiredValueChangedEvent)
-            .map((controlName) => this.getOccupiedWithFormGroup().controls[controlName])
-            .forEach((control) => {
-                control.setValidators(validators);
-                control.updateValueAndValidity();
-            })
-    }
-
-    private getOccupiedWithFormGroup() {
-        return (this.jobCancelForm.controls['occupiedWith'] as FormGroup)
     }
 
     ngOnDestroy(): void {
@@ -89,17 +71,13 @@ export class JobPublicationCancelDialogComponent implements OnInit, OnDestroy {
         const cancelRequest: JobCancellationRequest = {
             id: this.id,
             accessToken: this.accessToken,
-            positionOccupied: this.jobCancelForm.controls['positionOccupied'].value as boolean,
-            occupiedWithJobCenter: this.getOccupiedWithBooleanValue('jobCenter'),
-            occupiedWithPrivateAgency: this.getOccupiedWithBooleanValue('privateAgency'),
-            selfOccupied: this.getOccupiedWithBooleanValue('self')
+            positionOccupied: this.jobCancelForm.get('positionOccupied').value,
+            occupiedWithJobCenter: this.jobCancelForm.get('occupiedWith.jobCenter').value,
+            occupiedWithPrivateAgency: this.jobCancelForm.get('occupiedWith.privateAgency').value,
+            selfOccupied: this.jobCancelForm.get('occupiedWith.self').value
         };
         this.jobPublicationService
             .cancelJobPublication(cancelRequest)
             .subscribe((_) => this.activeModal.close())
-    }
-
-    private getOccupiedWithBooleanValue(key: string): boolean {
-        return this.getOccupiedWithFormGroup().controls[key].value as boolean
     }
 }
