94d37115d120f80a8c4d211ee49612c2da9ae29e
Michael Wirth <michael.wirth@mimacom.com>
2018-01-02 15:41:42 +0100

    Add monitoring (ELK and Zipkin)

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo 'publish to Artifactory'
-./gradlew bootWar publish -Pprod -Pgraphite -Pprometheus -x test --stacktrace
+./gradlew bootWar publish -Pprod -Pzipkin -x test --stacktrace
 echo 'deployment to DEV'
 curl "https://dev.job-room.ch:8443/deploy/jr2?$TRAVIS_REPO_SLUG&$TRAVIS_JOB_ID&$TRAVIS_JOB_NUMBER" --insecure
 echo 'deployment was successful'
