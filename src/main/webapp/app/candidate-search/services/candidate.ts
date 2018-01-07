dce7a6470c8bae2844468b637c8361d3cd548e43
Michael Wirth <michael.wirth@mimacom.com>
2018-01-07 13:29:53 +0100

    JR2-583 Add mobile number

--- src/main/webapp/app/candidate-search/services/candidate.ts
+++ src/main/webapp/app/candidate-search/services/candidate.ts
@@ -14,6 +14,7 @@ export interface Candidate {
     firstName: string;
     lastName: string;
     phone: string;
+    mobile: string;
     email: string;
     address: Address;
     nationalityCode: string;
