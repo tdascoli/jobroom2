bb4964a747472da87e462cb5d9018f351bd976e2
Michael Wirth <michael.wirth@mimacom.com>
2018-01-11 14:22:56 +0100

    JR2-680 Add cache interceptor

---
+++ src/main/webapp/app/blocks/interceptor/cache.interceptor.ts
@@ -0,0 +1,33 @@
+import { Observable } from 'rxjs/Observable';
+import {
+    BaseRequestOptions, RequestOptions, RequestOptionsArgs, Response,
+    URLSearchParams
+} from '@angular/http';
+import { JhiHttpInterceptor } from 'ng-jhipster';
+import { JhiBase64Service } from "ng-jhipster/src/service/base64.service";
+import { CookieService } from "ngx-cookie";
+
+export class CacheKeyInterceptor extends JhiHttpInterceptor {
+
+    constructor(private cookieService: CookieService, private base64: JhiBase64Service) {
+        super();
+    }
+
+    requestIntercept(options?: RequestOptionsArgs): RequestOptionsArgs {
+        const requestOptions = new RequestOptions(options);
+        if (!options) {
+            options = new BaseRequestOptions();
+        }
+        if (!requestOptions.params) {
+            requestOptions.params = new URLSearchParams();
+        }
+
+        requestOptions.params.set('_ng', this.base64.encode(this.cookieService.get('NG_TRANSLATE_LANG_KEY')));
+        options.params = requestOptions.params;
+        return options;
+    }
+
+    responseIntercept(observable: Observable<Response>): Observable<Response> {
+        return observable; // by pass
+    }
+}
--- src/main/webapp/app/blocks/interceptor/http.provider.ts
+++ src/main/webapp/app/blocks/interceptor/http.provider.ts
@@ -1,21 +1,24 @@
 import { JhiEventManager, JhiInterceptableHttp } from 'ng-jhipster';
 import { Injector } from '@angular/core';
-import { Http, XHRBackend, RequestOptions } from '@angular/http';
+import { Http, RequestOptions, XHRBackend } from '@angular/http';
 
 import { AuthInterceptor } from './auth.interceptor';
 import { LocalStorageService, SessionStorageService } from 'ng2-webstorage';
 import { AuthExpiredInterceptor } from './auth-expired.interceptor';
 import { ErrorHandlerInterceptor } from './errorhandler.interceptor';
 import { NotificationInterceptor } from './notification.interceptor';
+import { JhiBase64Service } from "ng-jhipster/src/service/base64.service";
+import { CookieService } from "ngx-cookie";
+import { CacheKeyInterceptor } from "./cache.interceptor";
 
-export function interceptableFactory(
-    backend: XHRBackend,
-    defaultOptions: RequestOptions,
-    localStorage: LocalStorageService,
-    sessionStorage: SessionStorageService,
-    injector: Injector,
-    eventManager: JhiEventManager
-) {
+export function interceptableFactory(backend: XHRBackend,
+                                     defaultOptions: RequestOptions,
+                                     localStorage: LocalStorageService,
+                                     sessionStorage: SessionStorageService,
+                                     injector: Injector,
+                                     eventManager: JhiEventManager,
+                                     cookieService: CookieService,
+                                     base64: JhiBase64Service) {
     return new JhiInterceptableHttp(
         backend,
         defaultOptions,
@@ -24,7 +27,8 @@ export function interceptableFactory(
             new AuthExpiredInterceptor(injector),
             // Other interceptors can be added here
             new ErrorHandlerInterceptor(eventManager),
-            new NotificationInterceptor(injector)
+            new NotificationInterceptor(injector),
+            new CacheKeyInterceptor(cookieService, base64)
         ]
     );
 }
@@ -39,7 +43,9 @@ export function customHttpProvider() {
             LocalStorageService,
             SessionStorageService,
             Injector,
-            JhiEventManager
+            JhiEventManager,
+            CookieService,
+            JhiBase64Service
         ]
     };
 }
