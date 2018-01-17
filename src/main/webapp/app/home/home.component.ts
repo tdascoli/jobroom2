d7646808c4d864515c661c36e0b0a0eeb7197c94
Thomas D'Ascoli <thomas@dasco.li>
2018-01-17 09:14:18 +0100

    JR2-540 fix backgroud images
    -Add background classes to the body element
    -Fixed background classes and upgrade styleguide

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 import {
@@ -12,11 +12,21 @@ import {
     JobSearchToolState
 } from './state-management';
 import { NgbTabChangeEvent } from '@ng-bootstrap/ng-bootstrap';
-import { ToolbarItem, CompaniesTab, AgenciesTab } from './state-management/state/layout.state';
+import {
+    AgenciesTab,
+    CompaniesTab,
+    ToolbarItem
+} from './state-management/state/layout.state';
 import { ActivatedRoute, Router } from '@angular/router';
 import { JobPublication } from '../shared/job-publication/job-publication.model';
 import { UserData } from './tools/job-publication-tool/service/user-data-resolver.service';
 import { getReset } from '../shared/state-management/state/core.state';
+import { Subscription } from 'rxjs/Subscription';
+
+const BACKGROUND_CLASS_NAME_ARRAY = [];
+BACKGROUND_CLASS_NAME_ARRAY[ToolbarItem.JOB_SEEKERS] = 'background--jobseeker';
+BACKGROUND_CLASS_NAME_ARRAY[ToolbarItem.COMPANIES] = 'background--companies';
+BACKGROUND_CLASS_NAME_ARRAY[ToolbarItem.RECRUITMENT_AGENCIES] = 'background--pea';
 
 @Component({
     selector: 'jhi-home',
@@ -25,7 +35,7 @@ import { getReset } from '../shared/state-management/state/core.state';
         'home.scss'
     ]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
     ToolbarItem: typeof ToolbarItem = ToolbarItem;
     CompaniesTab: typeof CompaniesTab = CompaniesTab;
     AgenciesTab: typeof AgenciesTab = AgenciesTab;
@@ -40,9 +50,12 @@ export class HomeComponent {
     reset$: Observable<number>;
     isSubnavCollapsed: boolean;
 
+    private subscription: Subscription;
+
     constructor(private store: Store<HomeState>,
                 private route: ActivatedRoute,
-                private router: Router) {
+                private router: Router,
+                private renderer: Renderer2) {
         this.jobSearchToolModel$ = store.select(getJobSearchToolState);
         this.candidateSearchToolModel$ = store.select(getCandidateSearchToolState);
         this.activeToolbarItem$ = store.select(getActiveToolbarItem);
@@ -59,6 +72,19 @@ export class HomeComponent {
             .map((data) => data['userData']);
     }
 
+    ngOnInit(): void {
+        this.subscription = this.activeToolbarItem$
+            .subscribe((toolbarItem: ToolbarItem) =>
+                this.addBackGroundClass(BACKGROUND_CLASS_NAME_ARRAY[toolbarItem]));
+    }
+
+    ngOnDestroy(): void {
+        this.removeAllBackgroundClass();
+        if (this.subscription) {
+            this.subscription.unsubscribe();
+        }
+    }
+
     selectCompaniesTab(event: NgbTabChangeEvent): void {
         const url = event.nextId === CompaniesTab.JOB_PUBLICATION ? '/companies/jobpublication' : '/companies/candidates';
         this.router.navigate([url]);
@@ -70,12 +96,22 @@ export class HomeComponent {
     }
 
     toggleSubnavbar() {
-        console.log('toggle');
-
         this.isSubnavCollapsed = !this.isSubnavCollapsed;
     }
 
     collapseSubnavbar() {
         this.isSubnavCollapsed = true;
+    }
+
+    private addBackGroundClass(className: string) {
+        this.removeAllBackgroundClass();
+        if (className) {
+            this.renderer.addClass(document.body, className);
+        }
+    }
+
+    private removeAllBackgroundClass(exclude = null) {
+        BACKGROUND_CLASS_NAME_ARRAY
+            .forEach((className) => this.renderer.removeClass(document.body, className));
     }
 }
