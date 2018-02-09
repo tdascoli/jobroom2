18bd806caa37284cee0e77457ed5dc853d39c48e
Valerii Rusakov <30697051+varusakov@users.noreply.github.com>
2018-02-09 11:26:28 +0200

    JR2-421 Fix employers filter reset issue

--- src/main/webapp/app/job-search/job-search-sidebar/job-search-filter/job-search-filter.component.ts
+++ src/main/webapp/app/job-search/job-search-sidebar/job-search-filter/job-search-filter.component.ts
@@ -26,13 +26,13 @@ export class JobSearchFilterComponent implements OnInit, OnDestroy {
     @Input()
     set reset(value: number) {
         if (value && this.filterForm && this.companyName) {
+            this.companyName.reset(this.searchFilter.companyName, { emitEvent: false });
             this.filterForm.reset({
                 contractType: this.searchFilter.contractType,
                 workingTime: this.searchFilter.workingTime,
                 onlineSince: this.searchFilter.onlineSince,
                 sort: this.searchFilter.sort
             });
-            this.companyName.reset(this.searchFilter.companyName, { emitEvent: false });
         }
     };
 
